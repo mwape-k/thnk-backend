426f6669dbc1f4e7712560a1445b4bf1891c2866
mwape-k <82158230+mwape-k@users.noreply.github.com>
2025-10-04 20:45:12 +0200

    CORE(init): Mongo initial set up

---
+++ config/db.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnfiedTopology: true,
+    });
+  } catch (error) {
+    console.log("MongoDB connection falied:", error.message);
+    process.exit(1);
+  }
+};
+module.exports = connectDB;
